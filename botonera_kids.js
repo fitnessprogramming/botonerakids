16792044132590afd593db037b97b3213696106f
fitnessprogramming <110104172+fitnessprogramming@users.noreply.github.com>
2022-08-03 00:17:26 -0300

    agregamos el sonido de la oveja

--- botonera_kids.js
+++ botonera_kids.js
@@ -1,4 +1,19 @@
 function sonido(animal) {
     
 }
-alert("ta linkeado")
+let click=0
+function playAudio(){
+    click=click+1
+    console.log(click);
+    let audio= new Audio("oveja001.mp3")
+//audio.src  ="COW2.mp3"
+audio.play("oveja001.mp3");
+//para cuando tengamos que poner en pausa un audio :)
+if(click==2){
+    click=0
+    console.log(click)
+}
+}
+
+let vaquita = document.querySelector(".vaca")
+console.log(vaquita)
